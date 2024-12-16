183b21d6b820667d33d5df028cf954ded8a552e9
ZX <zx@z-x.vip>
2024-12-17 00:37:21 +0800

    feat: 识别PDF文档锚点和大纲

--- types/Document.ts
+++ types/Document.ts
@@ -1,5 +1,22 @@
 // Document的定义是数内内容，而不是书本身，这样有利于控制数据传输，Document对于所有用户是相同的，而Book是用户拥有的数据结构
 
+import { CSSProperties } from 'react';
+import { Number2 } from '@/types/Math';
+
+// 文档锚（标记了文字id）
+export interface DocumentAnchorWithTextId {
+	pageId: string;
+	textId?: string;
+}
+
+// 文档锚（标记了位置）
+export interface DocumentAnchorWithPosition {
+	pageId: string;
+	position: Number2;
+}
+
+export type DocumentAnchor = DocumentAnchorWithPosition | DocumentAnchorWithTextId;
+
 export interface Document {
 	id: string;
 	pages: string[];
@@ -7,4 +24,14 @@ export interface Document {
 	version: number;
 	// 缩略图
 	thumbnail?: string;
+	// 大纲
+	outline: {
+		title: string;
+		style?: CSSProperties;
+		anchor?: DocumentAnchor;
+	}[];
+	// 元数据
+	metaData?: {
+		[key: string]: string | boolean | number | null;
+	};
 }
